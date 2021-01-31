9dd9615cfe79388a1b44ebc9e2618ffc57c418e8
ayushtenguria <46924317+ayushtenguria@users.noreply.github.com>
2021-01-31 18:41:15 +0530

    First Commit
    Initial Commit

---
+++ javascript.js
@@ -0,0 +1,7 @@
+document.getElementById('chk').addEventListener('change', () => {
+	document.body.classList.toggle('dark');
+});
+
+document.getElementById('chk').addEventListener('change', () => {
+	document.getElementsByClassName('card').toggle('dark');
+});
